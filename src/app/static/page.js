3b22fad49495ce03116ba8f49027a14e35d57cc1
Nesrinemh <nesrine.hadboun@live.fr>
2024-01-26 11:59:26 +0100

    first commit

--- src/app/static/page.js
+++ src/app/static/page.js
@@ -1,9 +1,9 @@
-"use client";
+'use client';
 
-import styles from "../page.module.css";
-import { useEffect, useState } from "react";
-import Content from "@/components/content";
-import { API_KEY } from "@/constants";
+import styles from '../page.module.css';
+import { useEffect, useState } from 'react';
+import Content from '@/components/content';
+import { API_KEY } from '@/constants';
 
 export default function Static() {
   const [data, setData] = useState(),
@@ -15,12 +15,11 @@ export default function Static() {
       setLoading(true);
       const response = await fetch(
         `https://api.nasa.gov/planetary/apod?api_key=${API_KEY}&count=1&thumbs=true`,
-        { cache: "no-cache" }
+        { cache: 'no-cache' }
       );
 
       if (!response.ok) {
-        // This will activate the closest `error.js` Error Boundary
-        throw new Error("Failed to fetch data");
+        throw new Error('Failed to fetch data');
       }
 
       const [json] = await response.json();
@@ -41,7 +40,11 @@ export default function Static() {
     <main className={styles.main}>
       <div className={styles.title}>CLIENT RENDERED</div>
       <button onClick={() => setInvalid(true)}>Reload</button>
-      {!loading && data ? <Content data={data} /> : <div>Loading...</div>}
+      {!loading && data ? (
+        <Content data={data} />
+      ) : (
+        <div>Loading...</div>
+      )}
     </main>
   );
 }
--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,1 @@
-export const API_KEY = "DEMO_KEY";
+export const API_KEY = 'kVxwDBjlUFIZ9Z9gLkxqvv7A3alztemOS3X4IGoR';
